9f5f3643f0030a05834f71b62f1ef701ae62efa8
Delphine <32301367+debraive@users.noreply.github.com>
2017-11-23 16:49:17 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
-<?php  
-echo 'Hello world'
-?>
+<?php
+try
+{
+    // On se connecte à MySQL
+    $pdo = new PDO('pgsql:host=ec2-46-137-174-67.eu-west-1.compute.amazonaws.com;', 'd52t7dq3hqt5cl', 'c569d62789084fb16e8b3314d2a52bb97e3f662c55548cab90be110fac2a719b');
+}
+catch(Exception $e)
+{
+    // En cas d'erreur, on affiche un message et on arrête tout
+        die('Erreur : '.$e->getMessage());
+}
+$query = 'INSERT INTO test (id, nom, prenom) VALUES (?, ?);';
+$prep = $pdo->prepare($query);
+ 
+$prep->bindValue(1, 'bertand', PDO::PARAM_STR);
+$prep->bindValue(2, 'ceci est un test pour desc', PDO::PARAM_STR);
+$prep->execute();
+$resultat = $pdo->query('SELECT * FROM test');
+while ($donnees = $resultat->fetch())
+{
+  echo '<br/>';
+  echo $donnees['nom'];
+  echo ' : ';
+  echo $donnees['description'];
+}
